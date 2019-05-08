5eea6d123676b0cc7f1a822af5014ed7d22ad682
v school <learn@vschool.io>
2019-05-08 14:22:05 -0600

    updating again

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App.js'
 import { BrowserRouter } from 'react-router-dom'
 import UserProvider from './context/UserProvider.js'
-import './style.css'
 
 
 ReactDOM.render(
